109adfd1c40a5c3ad5ceb4a817d5d833c80b0a26
SofiaDMurillo <sofiamurillo@gmail.com>
2025-07-08 14:34:58 +0200

    Updated API URL to deployed Render service

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 
-API_URL = "http://localhost:8000/predict"  
+API_URL = 'https://your-api-service.onrender.com/predict'
 
 st.title("ImmoEliza Price Prediction")
 
